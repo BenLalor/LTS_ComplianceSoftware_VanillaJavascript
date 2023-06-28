fd8b56ce854f7104bc75993972a73ca2afc2686c
BenLalor <49417236+BenLalor@users.noreply.github.com>
2023-06-28 17:44:17 -0600

    - Add database table - Flash message if already logged in

--- lts.py
+++ lts.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, Blueprint, request
+from flask import Flask, render_template, Blueprint, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-
+from flask_login import UserMixin
+import models  # THIS COULD BE WRONG BECAUSE I AM NOT USING THE WEBSSITE PACKAGE. See 1:30 in the video
 
 app = Flask(__name__)
 app.secret_key = "myKey123"
@@ -11,11 +12,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///{DB_NAME}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
-import models  # THIS COULD BE WRONG BECAUSE I AM NOT USING THE WEBSSITE PACKAGE. See 1:30 in the video
-
 
 # Represents the database table for user login information
-class users(db.Model):
+class users(db.Model, UserMixin):
     _id = db.Column("id", db.Integer, primary_key=True)
     email = db.Column(db.String(100))
     password = db.Column(db.String(100))
@@ -47,17 +46,6 @@ def comingSoon():
 # Path to login page
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    if request.method == "POST":
-        email = request.form.get("email")
-        password = request.form.get("password")
-        if users.query.filter_by(email=email).first() is not None:
-            # FIX ME - Call the function rather than directly rendering the template
-            return render_template("userAlreadyExists.html")
-        else:
-            usr = users(email, password)
-            db.session.add(usr)
-            db.session.commit()
-
     return render_template("login.html")
 
 
@@ -72,22 +60,35 @@ def logout():
 def signup():
     if request.method == "POST":
         email = request.form.get("email")
-        firstName = request.form.get("firstName")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
+        # FIX ME - Check to see if password 1 and password 2 are the same. If not, flash a warning an don't save to database
+        if users.query.filter_by(email=email).first() is not None:
+            flash("Account already exists with that email", category="error")
+            # FIX ME - Call the function rather than directly rendering the template
+            # return render_template("userAlreadyExists.html")
+        else:
+            flash("Account created!", category="success")
+            usr = users(email, password1)
+            db.session.add(usr)
+            db.session.commit()
+            return redirect(url_for("home"))
     return render_template("signup.html")
 
 
+# Path to view database data
 @app.route("/viewUsers")
 def viewUsers():
     return render_template("viewUsers.html", values=users.query.all())
 
 
+# Path to show that the user already exists
 @app.route("/userAlreadyExists")
 def userAlreadyExists():
     return render_template("userAlreadyExists.html")
 
 
+# Run the application
 if __name__ == "__main__":
     # create the database if it doesn't already exist
     db.create_all()
